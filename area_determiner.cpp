0775f5d00da85aff4175c74f07533a8260fb34ba
Ubuntu <ubuntu@ip-172-31-90-67.ec2.internal>
2019-11-24 22:49:28 +0000

    commit 1

---
+++ area_determiner.cpp
@@ -0,0 +1,51 @@
+// Copyright (c) 2019 St. Mother Teresa HS All rights reserved.
+//
+// Created by: Douglass Jeffrey
+// Created on: Nov 2019
+// This program calculates The area of a triangle using multiple functions
+
+#include <iostream>
+#include <string>
+
+
+float triangleCalc(float base, float height) {
+    // this function calculates area baseed of given base and height lengths
+    // process
+    float area = (base * height) / 2;
+    return area;
+}
+
+
+main() {
+    // this function recieves user inputs
+    // variables
+    std::string stringBase;
+    std::string stringHeight;
+    float base;
+    float height;
+    float area;
+
+    while (true) {
+        try {
+            // input
+            std::cout << "Input the base length of the triangle: " << std::endl;
+            std::cin >> stringBase;
+            std::cout << "Input the height of the triangle: " << std::endl;
+            std::cin >> stringHeight;
+            // turns variables into float which is needed for fractional values
+            base = std::stof(stringBase);
+            height = std::stof(stringHeight);
+
+            // runs triangleCalc()
+            area = triangleCalc(base, height);
+
+            // output
+            std::cout << "The area of your triangle is " << area <<
+                         " units squared."<< std::endl;
+            break;
+        } catch (std::invalid_argument) {
+            std::cout << "Invalid input. Try again." << std::endl;
+            continue;
+        }
+    }
+}
